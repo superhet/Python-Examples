2aea9d9c8de6c1203a4713ba904f6e818fc2bd0c
superhet <alois.taranetz@gmx.at>
2014-09-09 23:37:50 +0200

    Snippet for typecasting
    Snippet for typecasting

---
+++ for_loop_variable_step_size.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#Example of for-loop with variable step size
+"""
+Created on Tue Sep  9 22:43:32 2014
+
+@author: Alois_W
+"""
+#Highest reached number is 23
+#for i in range(1, 25, 2): 
+#    print (i)
+
+
+for i in range(1, 25, 4):
+    ausgabe=str(i)+"a"
+    print (ausgabe)
