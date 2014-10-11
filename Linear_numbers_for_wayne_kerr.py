91867a34be1a910cd92dcd222cfb16cc51feeea5
superhet <alois.taranetz@gmx.at>
2014-10-11 23:05:54 +0200

    Linear number gen for Inductor measurement

---
+++ Linear_numbers_for_wayne_kerr.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 30 22:00:21 2014
+
+@author: Alois_W
+"""
+import numpy as np
+
+trailingremoved = []
+#Set Precision
+
+#Put in start frequency [Hz]
+start_frequency=10000;
+stop_frequency=11001;
+divider=20;
+
+raw_values=np.linspace(start_frequency, stop_frequency, num=divider);
+#Elimniate decimal places
+without_decimals=np.around(raw_values, decimals=0);
+#Remove duplicates
+unique_values=np.unique(without_decimals);
+#Length of array
+Length=len(unique_values);
+
+for x in range(0, Length):
+    #Set Precision
+    t="{:.5E}".format(unique_values[x])
+    #Remove Trailing Zeros
+    while t[-1] == "0":
+        t = t[:-1]
+    trailingremoved.append(t)
+    print(t)
+#c='{:2E}'.format()
+#print(c)
